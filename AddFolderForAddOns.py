c37eee50c5b5d11c93f41f8e547efb4cb29e635c
Shem Page <shem@starter4ten.com>
2014-09-24 10:14:45 +1200

    use python directly not jenkins python plugin

---
+++ AddFolderForAddOns.py
@@ -0,0 +1,17 @@
+import os, sys, filecmp, shutil, glob, zipfile, fileinput, subprocess
+
+from mod_pbxproj import XcodeProject
+buildFolder = os.path.join(workspace, "CurrentBuild")
+projectToBuild = "HopScheduleTimer"
+addonsFolder = os.path.join(workspace, 'Addons')
+
+
+projectPath = os.path.join(buildFolder, projectToBuild + ".xcodeproj", "project.pbxproj")
+project = XcodeProject.Load(projectPath)
+project_group = project.get_or_create_group(projectToBuild)
+print("  Shem Adding all addons to the Xcode project...")
+project.add_folder(addonsFolder, project_group)
+
+if project.modified:
+    project.backup()
+    project.save()
